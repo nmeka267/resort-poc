6fcec39c723b9502e47b7d4a4a025d64801208ff
Meka <nmeka@deloitte.com>
2020-02-15 18:55:08 +0530

    Banner component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Error() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <link to="/" className="btn-primary">
+          return home
+        </link>
+      </Banner>
+    </Hero>
+  );
 }
